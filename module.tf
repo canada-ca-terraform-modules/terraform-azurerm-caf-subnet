6a554aed52fcb61b5b09dc7f2b14b8b2ddf30271
Bernard Maltais <bmaltais@gmail.com>
2020-06-25 17:49:58 +0000

    Fix local name issue

--- module.tf
+++ module.tf
@@ -1,12 +1,12 @@
 locals {
-  name-regex = "/[^0-9A-Za-z-_.]/" # Anti-pattern to match all characters not in: 0-9 a-z A-Z -
-  subnet-postfix = "_${var.subnetShortName}-snet"
+  name-regex             = "/[^0-9A-Za-z-_.]/" # Anti-pattern to match all characters not in: 0-9 a-z A-Z -
+  subnet-postfix         = "_${var.subnetShortName}-snet"
   subnet-postfix_replace = replace(local.subnet-postfix, "_", "-")
-  subnet-regex_compliant   = replace(local.subnet-postfix_replace, local.name-regex, "")
-  vnet-prefix = replace(var.virtual_network_name, "-vnet", "")
-  vnet-regex_compliant = replace(local.vnet-prefix, local.name-regex, "")
-  vnet-substr  = substr(local.name-regex_compliant, 0, 80 - length(local.subnet-regex_compliant))
-  subnet-fullName    = "${local.vnet-substr}${local.subnet-regex_compliant}"
+  subnet-regex_compliant = replace(local.subnet-postfix_replace, local.name-regex, "")
+  vnet-prefix            = replace(var.virtual_network_name, "-vnet", "")
+  vnet-regex_compliant   = replace(local.vnet-prefix, local.name-regex, "")
+  vnet-substr            = substr(local.vnet-regex_compliant, 0, 80 - length(local.subnet-regex_compliant))
+  subnet-fullName        = "${local.vnet-substr}${local.subnet-regex_compliant}"
 }
 
 resource "azurerm_subnet" "subnet" {
--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "resource_group_name" {
-  description = "(Required) Resource group name of the subnet to be created"    
+  description = "(Required) Resource group name of the subnet to be created"
 }
 
 variable "address_prefixes" {
-  description = "(Required) address_prefixes"  
+  description = "(Required) address_prefixes"
 }
 
 variable "virtual_network_name" {
- description = "(Required) virtual_network_name"
+  description = "(Required) virtual_network_name"
 }
 
 variable "subnetShortName" {
   description = "(Required) You can use a postfix to the name of the resource"
   type        = string
-  default = ""
+  default     = ""
 }
