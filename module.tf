52cc7d176f6b063e255194edce1eead5927b16ea
Bernard Maltais <bmaltais@gmail.com>
2020-06-27 15:17:29 +0000

    Adding support for RT and NSG association

--- module.tf
+++ module.tf
@@ -8,9 +8,21 @@ locals {
   subnet-fullName        = "${local.vnet-substr}${local.subnet-regex_compliant}"
 }
 
-resource "azurerm_subnet" "subnet" {
+resource azurerm_subnet subnet {
   name                 = local.subnet-fullName
   virtual_network_name = var.virtual_network_name
   resource_group_name  = var.resource_group_name
   address_prefixes     = var.address_prefixes
+}
+
+resource azurerm_subnet_route_table_association Project_MAZ-rta {
+  count = var.azurerm_route_table != null ? 1 : 0
+  subnet_id      = azurerm_subnet.subnet.id
+  route_table_id = var.azurerm_route_table.id
+}
+
+resource azurerm_subnet_network_security_group_association Project_OZ-nsg-Association {
+  count = var.network_security_group != null ? 1 : 0
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = var.network_security_group.id
 }
