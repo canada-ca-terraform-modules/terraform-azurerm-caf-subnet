aa4bd2f18bfb8e4d92df4d1f67e5ac99b4e0fcea
Bernard Maltais <bmaltais@gmail.com>
2020-06-27 18:57:21 +0000

    Updates

--- output.tf
+++ output.tf
@@ -2,7 +2,7 @@ output "object" {
   value       = azurerm_subnet.subnet
   description = "returns the full Azure Subnet Object"
 }
-
+/*
 output "resource_group_name" {
   value       = azurerm_subnet.subnet.resource_group_name
   description = "returns the resource_group_name of Subnet"
@@ -12,3 +12,4 @@ output "virtual_network_name" {
   value       = azurerm_subnet.subnet.virtual_network_name
   description = "returns the virtual_network_name of Subnet"
 }
+*/
