31e160d33bebbcb72f0a7221268272b3fe405122
Bernard Maltais <bmaltais@gmail.com>
2020-06-27 18:55:38 +0000

    Fix output

--- output.tf
+++ output.tf
@@ -3,16 +3,6 @@ output "object" {
   description = "returns the full Azure Subnet Object"
 }
 
-output "id" {
-  value       = azurerm_subnet.subnet.id
-  description = "returns the ID of Subnet"
-}
-
-output "name" {
-  value       = azurerm_subnet.subnet.name
-  description = "returns the name of Subnet"
-}
-
 output "resource_group_name" {
   value       = azurerm_subnet.subnet.resource_group_name
   description = "returns the resource_group_name of Subnet"
@@ -21,9 +11,4 @@ output "resource_group_name" {
 output "virtual_network_name" {
   value       = azurerm_subnet.subnet.virtual_network_name
   description = "returns the virtual_network_name of Subnet"
-}
-
-output "address_prefixes" {
-  value       = azurerm_subnet.subnet.address_prefixes
-  description = "returns the address_prefixes of Subnet"
 }
