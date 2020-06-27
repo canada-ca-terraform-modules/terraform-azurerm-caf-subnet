09492ff7b418e633727ea065474fc6ae855077fb
Bernard Maltais <bmaltais@gmail.com>
2020-06-27 16:53:49 +0000

    Handle many subnets

--- module.tf
+++ module.tf
@@ -1,29 +1,32 @@
 locals {
   name-regex             = "/[^0-9A-Za-z-_.]/" # Anti-pattern to match all characters not in: 0-9 a-z A-Z -
-  #subnet-postfix         = "_${var.subnetShortName}-snet"
-  #subnet-regex_compliant = replace(local.subnet-postfix, local.name-regex, "")
   vnet-prefix            = replace(var.virtual_network_name, "-vnet", "")
   vnet-regex_compliant   = replace(local.vnet-prefix, local.name-regex, "")
+  #subnet-postfix         = "_${var.userDefinedString}-snet"
+  #subnet-regex_compliant = replace(local.subnet-postfix, local.name-regex, "")
   #vnet-substr            = substr(local.vnet-regex_compliant, 0, 80 - length(local.subnet-regex_compliant))
-  subnet-fullName        = "${substr(local.vnet-regex_compliant, 0, 80 - length(replace("_${var.subnetShortName}-snet", local.name-regex, "")))}${replace("_${var.subnetShortName}-snet", local.name-regex, "")}"
   #subnet-fullName        = "${local.vnet-substr}${local.subnet-regex_compliant}"
+  subnet-fullName        = "${substr(local.vnet-regex_compliant, 0, 80 - length(replace("_${var.userDefinedString}-snet", local.name-regex, "")))}${replace("_${var.userDefinedString}-snet", local.name-regex, "")}"
 }
 
 resource azurerm_subnet subnet {
-  name                 = local.subnet-fullName
-  virtual_network_name = var.virtual_network_name
-  resource_group_name  = var.resource_group_name
-  address_prefixes     = var.address_prefixes
+  for_each = var.subnets
+  name                 = "${substr(local.vnet-regex_compliant, 0, 80 - length(replace("_${each.value.userDefinedString}-snet", local.name-regex, "")))}${replace("_${each.value.userDefinedString}-snet", local.name-regex, "")}"
+  virtual_network_name = var.virtual_network.name
+  resource_group_name  = var.resource_group.name
+  address_prefixes     = each.value.address_prefixes
 }
 
 resource azurerm_subnet_route_table_association route_table_association {
-  count          = var.route_table != null ? 1 : 0
-  subnet_id      = azurerm_subnet.subnet.id
-  route_table_id = var.route_table.id
+  for_each       = var.subnets
+  count          = each.value.route_table != null ? 1 : 0
+  subnet_id      = azurerm_subnet.subnet.[each.value.userDefinedString].id
+  route_table_id = each.value.route_table.id
 }
 
 resource azurerm_subnet_network_security_group_association network_security_group_association {
-  count                     = var.network_security_group != null ? 1 : 0
-  subnet_id                 = azurerm_subnet.subnet.id
-  network_security_group_id = var.network_security_group.id
+  for_each                  = var.subnets
+  count                     = each.value.network_security_group != null ? 1 : 0
+  subnet_id                 = azurerm_subnet.subnet.[each.value.userDefinedString].id
+  network_security_group_id = each.value.network_security_group.id
 }
--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
-variable "resource_group_name" {
-  description = "(Required) Resource group name of the subnet to be created"
+variable "resource_group" {
+  description = "(Required) Resource group object of the subnet to be created"
 }
 
 variable "address_prefixes" {
   description = "(Required) address_prefixes"
 }
 
-variable "virtual_network_name" {
-  description = "(Required) virtual_network_name"
+variable "virtual_network" {
+  description = "(Required) virtual_network object"
 }
 
-variable "subnetShortName" {
+variable "userDefinedString" {
   description = "(Required) You can use a postfix to the name of the resource"
   type        = string
   default     = ""
