ce246a7d2bf849821f12aeb351e6d686cfd8b3e4
Bernard Maltais <bmaltais@gmail.com>
2020-06-27 15:39:38 +0000

    Implementing single line name

--- module.tf
+++ module.tf
@@ -1,11 +1,12 @@
 locals {
   name-regex             = "/[^0-9A-Za-z-_.]/" # Anti-pattern to match all characters not in: 0-9 a-z A-Z -
-  subnet-postfix         = "_${var.subnetShortName}-snet"
-  subnet-regex_compliant = replace(local.subnet-postfix, local.name-regex, "")
+  #subnet-postfix         = "_${var.subnetShortName}-snet"
+  #subnet-regex_compliant = replace(local.subnet-postfix, local.name-regex, "")
   vnet-prefix            = replace(var.virtual_network_name, "-vnet", "")
   vnet-regex_compliant   = replace(local.vnet-prefix, local.name-regex, "")
-  vnet-substr            = substr(local.vnet-regex_compliant, 0, 80 - length(local.subnet-regex_compliant))
-  subnet-fullName        = "${local.vnet-substr}${local.subnet-regex_compliant}"
+  #vnet-substr            = substr(local.vnet-regex_compliant, 0, 80 - length(local.subnet-regex_compliant))
+  subnet-fullName        = "${substr(local.vnet-regex_compliant, 0, 80 - length(replace("_${var.subnetShortName}-snet", local.name-regex, "")))}${replace("_${var.subnetShortName}-snet", local.name-regex, "")}"
+  #subnet-fullName        = "${local.vnet-substr}${local.subnet-regex_compliant}"
 }
 
 resource azurerm_subnet subnet {
