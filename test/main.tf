2c84c54363452e7d0337f431a02fd43ddac68e49
bernardmaltais <bernard.maltais@canada.ca>
2021-04-01 14:13:24 -0400

    Update dev env (#6)
    * Update dev env
    
    * terraform-docs: automated action
    
    Co-authored-by: bernardmaltais <bernardmaltais@users.noreply.github.com>

--- test/main.tf
+++ test/main.tf
@@ -1,8 +1,14 @@
 terraform {
-  required_version = ">= 0.12.1"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=2.46.0"
+    }
+  }
+  required_version = ">= 0.14.1"
 }
+
 provider "azurerm" {
-  version = ">= 2.34.0"
   features {}
 }
 
