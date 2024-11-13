60833147f2d7bf5217f0057964c2972c3581682f
bernardmaltais <bernard.maltais@ssc-spc.gc.ca>
2024-11-13 08:02:07 -0500

    Update deprecated parameters

--- module.tf
+++ module.tf
@@ -1,17 +1,17 @@
 locals {
-  name-regex           = "/[^0-9A-Za-z-_.]/" # Anti-pattern to match all characters not in: 0-9 a-z A-Z -
-  vnet-prefix          = replace(var.virtual_network.name, "-vnet", "")
-  vnet-regex_compliant = replace(local.vnet-prefix, local.name-regex, "")
+  name-regex                                     = "/[^0-9A-Za-z-_.]/" # Anti-pattern to match all characters not in: 0-9 a-z A-Z -
+  vnet-prefix                                    = replace(var.virtual_network.name, "-vnet", "")
+  vnet-regex_compliant                           = replace(local.vnet-prefix, local.name-regex, "")
 }
 
 resource "azurerm_subnet" "subnet" {
   name                                          = "${substr(local.vnet-regex_compliant, 0, 80 - length(replace("_${var.subnet.userDefinedString}-snet", local.name-regex, "")))}${replace("_${var.subnet.userDefinedString}-snet", local.name-regex, "")}"
   virtual_network_name                          = var.virtual_network.name
   resource_group_name                           = var.resource_group.name
-  address_prefixes                              = lookup(var.subnet, "address_prefixes", null)
-  service_endpoints                             = lookup(var.subnet, "service_endpoints", null)
-  private_link_service_network_policies_enabled = lookup(var.subnet, "enforce_private_link_service_network_policies", false)
-  private_endpoint_network_policies_enabled     = lookup(var.subnet, "enforce_private_link_endpoint_network_policies", false)
+  address_prefixes                              = try(var.subnet.address_prefixes, null)
+  service_endpoints                             = try(var.subnet.service_endpoints, null)
+  private_link_service_network_policies_enabled = try(var.subnet.private_link_service_network_policies_enabled, false)
+  private_endpoint_network_policies             = try(var.subnet.private_endpoint_network_policies, "Disabled")
 
   dynamic "delegation" {
     for_each = lookup(var.subnet, "delegation", {}) != {} ? [1] : []
@@ -20,8 +20,8 @@ resource "azurerm_subnet" "subnet" {
       name = lookup(var.subnet.delegation, "name", null)
 
       service_delegation {
-        name    = lookup(var.subnet.delegation.service_delegation, "name", null)
-        actions = lookup(var.subnet.delegation.service_delegation, "actions", null)
+        name    = try(var.subnet.delegation.service_delegation.name, null)
+        actions = try(var.subnet.delegation.service_delegation.actions, null)
       }
     }
   }
