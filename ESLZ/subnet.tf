d5b5f90f207cb9bb6d141d181c964928b8f61323
Bernard Maltais <bernard.maltais@ssc-spc.gc.ca>
2026-03-27 08:15:10 -0400

    feat: add ESLZ/subnet.tf module block

---
+++ ESLZ/subnet.tf
@@ -0,0 +1,9 @@
+module "subnets" {
+  source   = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-subnet?ref=v3.2.0"
+  for_each = var.subnets
+
+  env             = var.env
+  resource_group  = local.resource_groups[each.value.resource_group]
+  virtual_network = local.virtual_networks[each.value.virtual_network]
+  subnet          = each.value
+}
